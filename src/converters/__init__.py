993de26de5fb3ac2a2954bc38ecd6ae17d150719
Zh-U-hB <Zhang-b25@tsinghua.org.cn>
2025-11-04 20:19:00 +0800

    Add FenestrationConverter to __all__ exports
    Add FenestrationConverter to __all__ exports

--- src/converters/__init__.py
+++ src/converters/__init__.py
@@ -2,10 +2,12 @@ from .base_converter import BaseConverter
 from .building_converter import BuildingConverter
 from .surface_converter import SurfaceConverter
 from .zone_converter import ZoneConverter
+from .fenestration_converter import FenestrationConverter
 
 __all__ = [
     "BaseConverter",
     "BuildingConverter",
     "SurfaceConverter",
-    "ZoneConverter"
+    "ZoneConverter",
+    "FenestrationConverter",
 ]
